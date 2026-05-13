ae46e2bf7a6e2c572e23b93749cf76ca232a411c
ZKRV <254007432+ZKRV02@users.noreply.github.com>
2026-05-13 21:51:18 +0300

    Оптимизировал

--- project.py
+++ project.py
@@ -23,7 +23,7 @@ X = df_combined[features]
 y = df_combined['label']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = RandomForestClassifier(n_estimators=100,n_jobs=-1, random_state=42)
+model = RandomForestClassifier(n_estimators=50,n_jobs=-1, random_state=42, max_depth=15)
 model.fit(X_train, y_train)
 
 predictions = model.predict(X_test)
