162e4a09a8d178fc7d83b9f7f9591f2b0a20303b
ZKRV <254007432+ZKRV02@users.noreply.github.com>
2026-05-13 20:12:40 +0300

    Add files via upload

---
+++ whitelist.py
@@ -0,0 +1,5 @@
+WHITELIST = [
+    "google.com", "youtube.com", "github.com",
+    "leetcode.com", "stackoverflow.com", "wikipedia.org",
+    "vk.com", "yandex.ru", "mail.ru", "kremlin.ru", "leetcode.com", "claude.ai"
+]
