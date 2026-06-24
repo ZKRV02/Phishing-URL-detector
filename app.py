17e41d1c759d3487ad5d945803e2c3a0bb9ced64
ZKRV <254007432+ZKRV02@users.noreply.github.com>
2026-06-24 23:46:38 +0300

    i forgot folder directory

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 import re
 from whitelist import *
 
-with open('maybefinallysomeimprovement/model.pkl', 'rb') as f:
+with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 def is_whitelisted(url):
